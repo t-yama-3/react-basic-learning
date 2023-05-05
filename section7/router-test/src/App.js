4d89700ea54e2757ab97f1c4bf87a0097c82d124
t-takayama <pxbdg597@ybb.ne.jp>
2023-05-05 17:02:13 +0900

    Lesson7 Navigate リダイレクト

--- section7/router-test/src/App.js
+++ section7/router-test/src/App.js
@@ -3,18 +3,19 @@
 import Home from './pages/Home';
 import Information from './pages/Information';
 import Contact from './pages/Contact';
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import { Routes, Route, Link, NavLink, Navigate } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import './App.css'
 
 function App() {
+  const isAuthenticated = true
   return (
     <>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/information">Information</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/contact">Contact</Link></li><br />
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/information">Information</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
@@ -25,6 +26,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/information" element={<Information />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/auth" element={isAuthenticated ? <Navigate to="/contact" /> : <Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
