c389a9bf3fa7d7ed9d4ec5453f22122c812ea810
t-takayama <pxbdg597@ybb.ne.jp>
2023-05-05 16:56:50 +0900

    Lesson7 useNavigate

--- section7/router-test/src/pages/Information.js
+++ section7/router-test/src/pages/Information.js
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 const Information = () => {
+  const navigate = useNavigate()
+  const goToClick = () => navigate('/contact')
   return (
     <>
       <h2>Information です</h2>
+      <button onClick={goToClick}>お問い合わせページ</button>
     </>
   )
 }
