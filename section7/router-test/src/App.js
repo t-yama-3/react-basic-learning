0b73d8596302f9fab4e6538770704fa353b6c682
t-takayama <pxbdg597@ybb.ne.jp>
2023-05-05 16:46:44 +0900

    Lesson7 Link

--- section7/router-test/src/App.js
+++ section7/router-test/src/App.js
@@ -3,12 +3,24 @@
 import Home from './pages/Home';
 import Information from './pages/Information';
 import Contact from './pages/Contact';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
 import NotFound from './pages/NotFound';
+import './App.css'
 
 function App() {
   return (
     <>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/information">Information</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/information">Information</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+      <a href='information'>aタグです</a>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/information" element={<Information />} />
