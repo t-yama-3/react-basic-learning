8be76736f7427c9a80fef71ab5ae1e351756a574
t-takayama <pxbdg597@ybb.ne.jp>
2023-05-06 14:32:47 +0900

    book-app

---
+++ section8/book-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import MuiTest from './pages/MuiTest';
+
+function App() {
+  return (
+    <>
+      <MuiTest />
+    </>
+  );
+}
+
+export default App;
---
+++ section8/book-app/src/pages/MuiTest.js
@@ -0,0 +1,11 @@
+import MuiButton from "../components/MuiButton"
+
+const MuiTest = () => {
+  return (
+    <>
+      <MuiButton />
+    </>
+  )
+}
+
+export default MuiTest
